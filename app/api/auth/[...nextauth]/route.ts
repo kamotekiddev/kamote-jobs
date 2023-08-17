d51a18cfd7730f83ccd1077abed6164ead95bf1e
kamote-kid <kamote-kid@gmail.com>
2023-08-17 19:17:43 +0800

    extract auth options to a separate file

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,48 +1,6 @@
-import bcrypt from 'bcrypt';
-import NextAuth, { AuthOptions } from 'next-auth';
-import CredentialsProvider from 'next-auth/providers/credentials';
-import GoogleProvider from 'next-auth/providers/google';
-import { PrismaAdapter } from '@next-auth/prisma-adapter';
+import NextAuth from 'next-auth';
 
-import client from '@/lib/prismadb';
-
-export const authOptions: AuthOptions = {
-    adapter: PrismaAdapter(client),
-    providers: [
-        GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID!,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-        }),
-        CredentialsProvider({
-            name: 'credentials',
-            credentials: {
-                email: { label: 'email', type: 'text' },
-                password: { label: 'password', type: 'password' },
-            },
-            async authorize(credentials) {
-                if (!credentials?.email || !credentials?.password) return null;
-
-                const user = await client.user.findUnique({
-                    where: { email: credentials?.email },
-                });
-
-                if (!user || !user?.hashedPassword) return null;
-
-                const correctPassword = await bcrypt.compare(
-                    credentials.password,
-                    user.hashedPassword
-                );
-
-                if (!correctPassword) return null;
-                return user;
-            },
-        }),
-    ],
-    debug: true,
-    session: { strategy: 'jwt' },
-    secret: process.env.NEXTAUTH_SECRET,
-    pages: { signIn: '/sign-in' },
-};
+import authOptions from '@/lib/authOptions';
 
 const handler = NextAuth(authOptions);
 
