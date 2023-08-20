0c82ca20f2dd567dabe5856d19c7042544c7b5fc
kamote-kid <kamote-kid@gmail.com>
2023-08-20 11:54:43 +0800

    connect roles to jobpost on creation

--- actions/createJobPost.ts
+++ actions/createJobPost.ts
@@ -4,16 +4,18 @@ import getErrorMessage from '@/lib/getErrorMessage';
 import getCurrentUser from './getCurrentUser';
 import client from '@/lib/prismadb';
 
-type Params = Pick<JobPost, 'caption' | 'image'> & { roles: string[] };
+type Params = Pick<JobPost, 'caption' | 'image' | 'title'> & {
+    roles: string[];
+};
 
-const createJobPost = async ({ caption, image, roles }: Params) => {
+const createJobPost = async ({ caption, image, roles, title }: Params) => {
     try {
-        const roleIds: string[] = [];
         const currentUser = await getCurrentUser();
         if (!currentUser?.id) throw new Error('Unauthorized');
-        if (!caption || roles.length <= 0) throw new Error('Invalid data');
+        if (!caption || roles.length <= 0 || !title)
+            throw new Error('Invalid data');
 
-        roles.forEach(async (currentRole) => {
+        const promises = roles.map(async (currentRole) => {
             let role = await client.jobRole.findFirst({
                 where: { role: currentRole },
             });
@@ -23,11 +25,24 @@ const createJobPost = async ({ caption, image, roles }: Params) => {
                     data: { role: currentRole },
                 });
             }
-            roleIds.push(role?.id!);
+            return role.id;
         });
 
+        const roleIds = await Promise.all(promises);
+
         const newJobPost = await client.jobPost.create({
-            data: { caption, image, roleIds },
+            data: {
+                caption,
+                image,
+                roleIds,
+                title,
+                userId: currentUser.id,
+                roles: {
+                    connect: [...roleIds.map((rolId) => ({ id: rolId }))],
+                },
+            },
+
+            include: { roles: true },
         });
         return { newJobPost };
     } catch (error) {
