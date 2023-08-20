3aa2dbb1bd7e9be1a9983bfe1baa023cd315dc4a
kamote-kid <kamote-kid@gmail.com>
2023-08-20 22:40:25 +0800

    feature: add header on users recruitments

--- app/(general)/jobs/recruitments/page.tsx
+++ app/(general)/jobs/recruitments/page.tsx
@@ -2,14 +2,17 @@ import getUserRecruitmentPosts from '@/actions/getUserRecruitmentPosts';
 import RecruitmentLists from './components/RecruitmentLists';
 
 const Recruitments = async () => {
-    const { jobposts, error } = await getUserRecruitmentPosts();
-
-    if (error) return <span>{error}</span>;
-    if (!jobposts?.length) return <span>No data found.</span>;
+    const { jobposts } = await getUserRecruitmentPosts();
 
     return (
-        <section className='grid grid-cols-[1fr_300px]'>
-            <RecruitmentLists jobPosts={jobposts} />
+        <section className='grid grid-cols-[1fr_300px] overflow-auto scrollbar-hide'>
+            <div className='space-y-4'>
+                <section className='sticky top-0 rounded-lg border bg-white p-4'>
+                    <h1 className='text-2xl font-black'>Your Recruitments</h1>
+                    <p>List of your recruitments Notice</p>
+                </section>
+                <RecruitmentLists jobPosts={jobposts} />
+            </div>
         </section>
     );
 };
