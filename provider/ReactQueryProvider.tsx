55cdecfd0fa44a1653dff61e3a885ee1f228942e
kamote-kid <kamote-kid@gmail.com>
2023-08-24 21:27:09 +0800

    install react query devtools

--- provider/ReactQueryProvider.tsx
+++ provider/ReactQueryProvider.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import React, { ReactNode } from 'react';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { QueryClient, QueryClientProvider } from 'react-query';
 
 type Props = { children: ReactNode };
@@ -16,6 +17,7 @@ const queryClient = new QueryClient({
 const ReactQueryProvider = ({ children }: Props) => {
     return (
         <QueryClientProvider client={queryClient}>
+            <ReactQueryDevtools initialIsOpen={false} />
             {children}
         </QueryClientProvider>
     );
