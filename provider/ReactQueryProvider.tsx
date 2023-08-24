ce70144ac652959aface92a2dceeec1e751cd9f7
kamote-kid <kamote-kid@gmail.com>
2023-08-24 22:06:17 +0800

    remove react query devtools

--- provider/ReactQueryProvider.tsx
+++ provider/ReactQueryProvider.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import React, { ReactNode } from 'react';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { QueryClient, QueryClientProvider } from 'react-query';
 
 type Props = { children: ReactNode };
@@ -9,7 +8,7 @@ type Props = { children: ReactNode };
 const queryClient = new QueryClient({
     defaultOptions: {
         queries: {
-            staleTime: 10 * (60 * 1000), // 10 mins
+            staleTime: 10 * (60 * 1000),
         },
     },
 });
@@ -17,7 +16,6 @@ const queryClient = new QueryClient({
 const ReactQueryProvider = ({ children }: Props) => {
     return (
         <QueryClientProvider client={queryClient}>
-            <ReactQueryDevtools initialIsOpen={false} />
             {children}
         </QueryClientProvider>
     );
