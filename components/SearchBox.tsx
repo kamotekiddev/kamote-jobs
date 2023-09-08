22544605eb98e738f17c149376cb799ccfb2b976
kamote-kid <kamote-kid@gmail.com>
2023-09-08 21:10:27 +0800

    feature: focus on search whenever the container is clicked

--- components/SearchBox.tsx
+++ components/SearchBox.tsx
@@ -1,19 +1,39 @@
 'use client';
 
-import { Search } from 'lucide-react';
+import { useRef } from 'react';
+import { Search, X } from 'lucide-react';
+import { useRouter } from 'next/navigation';
 import useSearchTextStore from '@/hooks/useSearchText';
 
 const SearchBox = () => {
-    const { searchQuery, onChange } = useSearchTextStore();
+    const router = useRouter();
+    const inputRef = useRef<HTMLInputElement>(null);
+    const { searchQuery, onChange, clearSearchQuery } = useSearchTextStore();
+
+    const handleSearchClick = () => {
+        inputRef.current?.focus();
+        router.push('/jobs');
+    };
 
     return (
-        <section className='roundef-full flex items-center gap-2 rounded-full bg-slate-100 px-2 pl-3 focus-within:ring-1 focus-within:ring-black'>
-            <Search />
+        <section
+            onClick={handleSearchClick}
+            className='relative flex w-full items-center gap-2 rounded-full bg-slate-100 px-3 pr-8 transition-all duration-300 ease-in-out focus-within:ring-1 focus-within:ring-black'
+        >
+            <Search className='h-4 w-4' />
             <input
+                ref={inputRef}
+                placeholder='Search Jobs...'
                 value={searchQuery}
                 onChange={(e) => onChange(e.target.value)}
                 className='bg-transparent p-2 focus:outline-none'
             />
+            {searchQuery && (
+                <X
+                    className='absolute right-4 h-4 w-4 cursor-pointer'
+                    onClick={clearSearchQuery}
+                />
+            )}
         </section>
     );
 };
