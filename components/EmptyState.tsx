9d6cdbe2560b42b31c4945e693441c090723b6af
kamote-kid <kamote-kid@gmail.com>
2023-09-11 20:15:12 +0800

    improve the empty state

--- components/EmptyState.tsx
+++ components/EmptyState.tsx
@@ -1,8 +1,12 @@
-type Props = { title?: string };
+import { ReactNode } from 'react';
+import { HardDrive } from 'lucide-react';
 
-const EmptyState = ({ title = 'No Record Found.' }: Props) => {
+type Props = { title?: string; icon?: ReactNode };
+
+const EmptyState = ({ title = 'No Record Found.', icon }: Props) => {
     return (
-        <section className='rounded-lg border bg-white p-4 shadow-sm'>
+        <section className='space-y-4 rounded border bg-white p-4 text-slate-700 shadow-sm'>
+            {icon || <HardDrive className='mb-4 h-10 w-10' />}
             {title}
         </section>
     );
