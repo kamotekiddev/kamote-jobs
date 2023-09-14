e02ca2b977ab8556665f1b741d674c0953c8d28a
kamote-kid <kamote-kid@gmail.com>
2023-09-14 20:34:17 +0800

    feature: remove the header

--- app/(pages)/jobs/owned/page.tsx
+++ app/(pages)/jobs/owned/page.tsx
@@ -1,23 +1,21 @@
 import Link from 'next/link';
+import { Button } from '@/components/ui/button';
+
 import getMyJobList from '@/actions/getMyJoblist';
 import OwnedJobList from './components/OwnedJobList';
-import { Button } from '@/components/ui/button';
 import HiringStatusFilter from '@/components/HiringStatusFilter';
 
 const OwnedJobs = async () => {
     const { jobList } = await getMyJobList();
 
     return (
-        <section className='overflow-auto scrollbar-hide'>
+        <section className='overflow-auto p-2 scrollbar-hide'>
             <div className='space-y-4'>
-                <header className='sticky top-0 z-40 flex items-center justify-between rounded-lg border bg-white p-4'>
-                    <h1 className='text-xl font-bold'>My Posts</h1>
-                    <div className='flex items-center gap-2'>
-                        <Link href='/jobs/create'>
-                            <Button size='sm'>Create</Button>
-                        </Link>
-                        <HiringStatusFilter />
-                    </div>
+                <header className='flex items-center gap-2'>
+                    <HiringStatusFilter />
+                    <Link href='/jobs/create'>
+                        <Button size='sm'>Create</Button>
+                    </Link>
                 </header>
                 <OwnedJobList initialJobList={jobList} />
             </div>
